bff11cfdf884a25554dfab665eccfe9dce88366d
RosaCielo <ashyzan@gmail.com>
2024-06-26 19:20:59 +0200

    Correction
    add @Controller

--- maven/best-of-the-year/src/main/java/it/ashyzan/boy/restcontroller/MvcControllerBoy.java
+++ maven/best-of-the-year/src/main/java/it/ashyzan/boy/restcontroller/MvcControllerBoy.java
@@ -1,9 +1,10 @@
 package it.ashyzan.boy.restcontroller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@Controller
 public class MvcControllerBoy {
 	
 	@GetMapping ("/best")
