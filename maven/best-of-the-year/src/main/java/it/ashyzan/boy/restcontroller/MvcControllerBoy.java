b84d25217383551e9f9832b60b6732e6740abdb7
RosaCielo <ashyzan@gmail.com>
2024-07-08 18:53:26 +0200

    finish movie controller java application and themyleaf + add css
    added CSS style boostrap to mani pages: - http://localhost:8080/bestmovies
    - http://localhost:8080/movies/$ID
    - http://localhost:8080/moviestest

--- maven/best-of-the-year/src/main/java/it/ashyzan/boy/restcontroller/MvcControllerBoy.java
+++ maven/best-of-the-year/src/main/java/it/ashyzan/boy/restcontroller/MvcControllerBoy.java
@@ -7,31 +7,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.ashyzan.boy.model.Movie;
+
 @Controller
 public class MvcControllerBoy {
-	
-	@GetMapping ("/best")
-	public String best(@RequestParam(name = "nome", required = false) 
-	String nome, Model model) {
-		
+
+	@GetMapping("/best")
+	public String best(@RequestParam(name = "nome", required = false) String nome, Model model) {
+
 		model.addAttribute("name", nome);
-		
+
 		return "best";
-		
+
 	}
-	
-	
-	
-	@GetMapping ("/moviestest")
-	public String getMoviesTest(Model model){
+
+	@GetMapping("/moviestest")
+	public String getMoviesTest(Model model) {
 		Movie filmUno = new Movie();
 		Movie filmDue = new Movie();
 		Movie filmTre = new Movie();
-		
+
 		filmUno.setTitleMovie("Il quinto elemento");
 		filmUno.setAuthorMovie("Luc Besson");
 		filmUno.setIdMovie(0);
@@ -41,49 +38,45 @@ public class MvcControllerBoy {
 		filmTre.setTitleMovie("Matrix");
 		filmTre.setAuthorMovie("Fratelli Wachoski");
 		filmTre.setIdMovie(2);
-		
+
 		List<Movie> bestmoviesList = new ArrayList<>();
-		
+
 		bestmoviesList.add(filmUno);
 		bestmoviesList.add(filmDue);
-		bestmoviesList.add(filmTre); 
+		bestmoviesList.add(filmTre);
 
 		model.addAttribute("movie", filmUno);
-		
-		model.addAttribute("movieList",bestmoviesList);
-		
-		return "moviestest";
-		
-	}
-	
-	@GetMapping ("/bestmovies")
-	public String getBestMovies(Model model){
 
-		
-		model.addAttribute("movieList", getBestMovies());
-		
-		return "bestmovies";
-		
+		model.addAttribute("movieList", bestmoviesList);
+
+		return "moviestest";
+
 	}
-	
-	
-	 
-	
+
+	@GetMapping("/bestmovies")
+	public String getBestMovies(Model model) {
+
+		model.addAttribute("movieList", getBestMovies());
+		// model.addAttribute("idMovie", getIdMovie());
+
+		return "bestmovies";
+
+	}
+
 	@GetMapping("/movies/{id}")
 	public String show(@PathVariable("id") Integer idMovie, Model model) {
-		
+
 		model.addAttribute("idIntero", idMovie);
 		model.addAttribute("movieList", getBestMovies());
-		
+
 		return "/movies";
 	}
 
-	
 	private List<Movie> getBestMovies() {
 		Movie filmUno = new Movie();
 		Movie filmDue = new Movie();
 		Movie filmTre = new Movie();
-		
+
 		filmUno.setTitleMovie("Il quinto elemento");
 		filmUno.setAuthorMovie("Luc Besson");
 		filmUno.setIdMovie(0);
@@ -93,20 +86,14 @@ public class MvcControllerBoy {
 		filmTre.setTitleMovie("Matrix");
 		filmTre.setAuthorMovie("Fratelli Wachoski");
 		filmTre.setIdMovie(2);
-		
+
 		List<Movie> bestmoviesList = new ArrayList<>();
-		
+
 		bestmoviesList.add(filmUno);
 		bestmoviesList.add(filmDue);
 		bestmoviesList.add(filmTre);
-		
+
 		return bestmoviesList;
 	}
-	
-
-	
-	
-	
-
 
 }
